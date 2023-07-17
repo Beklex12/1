8ae3cfa635e97b4d380666bad02ee3155effdc12
Beklex12 <139662301+Beklex12@users.noreply.github.com>
2023-07-17 21:11:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-let products = document.querySelectorAll('.product');
-let cartItems = [];
-
-products.forEach((product) => {
-    let addButton = product.querySelector('button');
-    addButton.addEventListener('click', () => {
-        let productName = product.querySelector('h3').textContent;
-        let productPrice = product.querySelector('p').textContent;
-        let cartItem = {
-            name: productName,
-            price: productPrice
-        };
-        cartItems.push(cartItem);
-        console.log(cartItems);
+document.addEventListener("DOMContentLoaded", () => {
+    const balls = document.querySelectorAll(".ball");
+  
+    balls.forEach(ball => {
+      moveBall(ball);
     });
-});
+  });
+  
+  function moveBall(ball) {
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const ballWidth = ball.offsetWidth;
+    const ballHeight = ball.offsetHeight;
+  
+    function getRandomAngle() {
+      return Math.random() * 2 * Math.PI;
+    }
+  
+    function getRandomSpeed() {
+      return 1 + Math.random() * 3; // Adjust the speed range as needed
+    }
+  
+    let posX = Math.random() * (screenWidth - ballWidth);
+    let posY = Math.random() * (screenHeight - ballHeight);
+  
+    let angle = getRandomAngle();
+    let speed = getRandomSpeed();
+  
+    function updatePosition() {
+      posX += speed * Math.cos(angle);
+      posY += speed * Math.sin(angle);
+  
+      // Wrap the balls around the screen edges
+      if (posX > screenWidth) posX = -ballWidth;
+      if (posX < -ballWidth) posX = screenWidth;
+      if (posY > screenHeight) posY = -ballHeight;
+      if (posY < -ballHeight) posY = screenHeight;
+  
+      ball.style.left = `${posX}px`;
+      ball.style.top = `${posY}px`;
+  
+      requestAnimationFrame(updatePosition);
+    }
+  
+    updatePosition();
+  }
+  
